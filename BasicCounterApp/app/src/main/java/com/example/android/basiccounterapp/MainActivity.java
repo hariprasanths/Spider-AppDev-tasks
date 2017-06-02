be140e12ae333218977b433ec836eda1bd5bf725
Hariprasanth <shhariprasanth@gmail.com>
2017-06-02 14:01:44 +0530

    .java file is modified by writing code for the button

--- BasicCounterApp/app/src/main/java/com/example/android/basiccounterapp/MainActivity.java
+++ BasicCounterApp/app/src/main/java/com/example/android/basiccounterapp/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.android.basiccounterapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        display(i);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                display(++i);
+            }
+        });
+    }
+    void display(int i)
+    {
+        TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setText(String.valueOf(i));
     }
 }
