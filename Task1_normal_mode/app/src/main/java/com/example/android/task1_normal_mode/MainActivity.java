1379fbe83de5b6a76608c9db0377c61702495d95
Hariprasanth <shhariprasanth@gmail.com>
2017-06-11 16:00:13 +0530

    created a new activity and linked it using an intent

--- Task1_normal_mode/app/src/main/java/com/example/android/task1_normal_mode/MainActivity.java
+++ Task1_normal_mode/app/src/main/java/com/example/android/task1_normal_mode/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.task1_normal_mode;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +14,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String STRINGTEXT = "com.example.myfirstapp.MESSAGE";
     ArrayList<String> arrayList = new ArrayList<String>();
     String inputText;
     ArrayAdapter<String> listAdapter;
@@ -23,23 +26,32 @@ public class MainActivity extends AppCompatActivity {
         final EditText addTextbox = (EditText) findViewById(R.id.add_textbox);
         Button addButton = (Button) findViewById(R.id.add_button);
         final ListView listView = (ListView) findViewById(R.id.list);
-        listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
         listView.setAdapter(listAdapter);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 inputText = addTextbox.getText().toString();
-                if(inputText.equals(""))
-                    Toast.makeText(getApplicationContext(),"Enter the text and then click add",Toast.LENGTH_SHORT).show();
-                else
-                {
+                if (inputText.equals(""))
+                    Toast.makeText(getApplicationContext(), "Enter the text and then click add", Toast.LENGTH_SHORT).show();
+                else {
                     arrayList.add(inputText);
                     listAdapter.notifyDataSetChanged();
-                    addTextbox.getText().clear();}
+                    addTextbox.getText().clear();
+                }
             }
         });
 
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(MainActivity.this, SecondActivty.class);
+                intent.putExtra(STRINGTEXT, listView.getItemAtPosition(position).toString());
+                startActivity(intent);
+            }
+        });
     }
 }
