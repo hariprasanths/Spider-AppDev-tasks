851f04383aba05d04a09b9fa93d47423581f29a5
Hariprasanth <shhariprasanth@gmail.com>
2017-06-11 15:15:29 +0530

    list with add button is created

---
+++ Task1_normal_mode/app/src/main/java/com/example/android/task1_normal_mode/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.task1_normal_mode;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> arrayList = new ArrayList<String>();
+    String inputText;
+    ArrayAdapter<String> listAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText addTextbox = (EditText) findViewById(R.id.add_textbox);
+        Button addButton = (Button) findViewById(R.id.add_button);
+        final ListView listView = (ListView) findViewById(R.id.list);
+        listAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(listAdapter);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                inputText = addTextbox.getText().toString();
+                if(inputText.equals(""))
+                    Toast.makeText(getApplicationContext(),"Enter the text and then click add",Toast.LENGTH_SHORT).show();
+                else
+                {
+                    arrayList.add(inputText);
+                    listAdapter.notifyDataSetChanged();
+                    addTextbox.getText().clear();}
+            }
+        });
+
+
+    }
+}
